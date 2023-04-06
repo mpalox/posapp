3af860954b29069888533563d956b3a11ebafc51
mpalox <palomustafa@gmail.com>
2023-04-06 01:30:16 +0100

    tum kutuphaler yuklendi

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-
+import { Button} from 'antd';
 function App() {
   return (
     <div>
-      hello
+      <Button type="primary">Primary Button</Button>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
     </div>
   );
