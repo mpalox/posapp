f111de91f379f1861a04d3bd0d477a784d506dd9
mpalox <palomustafa@gmail.com>
2023-04-06 23:52:42 +0100

    kategori kismi bitti

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
+import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
-
-
-
 function App() {
   return (
-    <div>
+    <>
       <Header/>
-      
-    </div>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories/>
+        </div>
+        <div className="flex-[8]">
+          Products        
+        </div>
+        <div className="">
+          <div>Cart Totals</div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,33 @@
+const Categories = () => {
+  return (
+    <ul className="flex gap-4 flex-col text-lg">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Tumu</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Yiyecek</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Icecek</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Icecek</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Icecek</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Icecek</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Icecek</span>
+        </li>
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+            <span>Icecek</span>
+        </li>
+        
+    </ul>
+  )
+}
+
+export default Categories
