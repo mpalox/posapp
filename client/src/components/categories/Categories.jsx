eab24bafb0f75647c8601984bf96a413145ccaa2
mpalox <palomustafa@gmail.com>
2023-04-07 01:16:08 +0100

    sepet bolumu tamamlandi

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,28 +1,28 @@
 const Categories = () => {
   return (
     <ul className="flex gap-4 flex-col text-lg">
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Tumu</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Yiyecek</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Icecek</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Icecek</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Icecek</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Icecek</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Icecek</span>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
             <span>Icecek</span>
         </li>
         
