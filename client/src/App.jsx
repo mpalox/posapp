49367c5a76545b144dd756bf52518ca931c5e909
mpalox <palomustafa@gmail.com>
2023-04-08 01:12:36 +0100

    React dom kurulumu ve islmler tamalandi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,16 @@
-import CartTotals from "./components/cart/CartTotals";
-import Categories from "./components/categories/Categories";
-import Header from "./components/header/Header";
-import Products from "./components/products/Products";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 function App() {
   return (
-    <>
-      <Header/>
-      <div className="home px-6 flex flex-col md:flex-row justify-between gap-10 md:pb-0 pb-24">
-        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-          <Categories/>
-        </div>
-        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products/>        
-        </div>
-        <div className="card-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border ">          
-              <CartTotals/>         
-        </div>
-
-      </div>
-    </>
-  );
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/cart" element={<CartPage/>}/>
+        </Routes>
+    </BrowserRouter>
+   
+  )
 }
 
 export default App;
