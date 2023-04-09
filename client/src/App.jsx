990258e40d1773fc8b33f3f4a036f9d816c2c76a
mpalox <palomustafa@gmail.com>
2023-04-09 14:18:45 +0100

    musteriler ve faturalari bolumu tamamlandi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import BillPage from "./pages/BillPage"
+import CostumerPage from "./pages/CostumerPage";
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/cart" element={<CartPage/>}/>
+            <Route path="/bills" element={<BillPage/>}/>
+            <Route path="/costumers" element={<CostumerPage/>}/>
         </Routes>
     </BrowserRouter>
    
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -32,11 +32,11 @@ const Header = () => {
                   <span className='md:text-xs text-[10px]'>Sepet</span>                            
               </Link>
             </Badge>
-            <Link to={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>              
+            <Link to={"/bills"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>              
                 <CopyOutlined  className='md:text-2xl text-xl' />
                 <span className='md:text-xs text-[10px]'>Faturalar</span>             
             </Link>
-            <Link to={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>              
+            <Link to={"/costumers"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>              
                 <UserOutlined className='md:text-2xl text-xl' />
                 <span className='md:text-xs text-[10px]'>Musteriler</span>             
             </Link>
