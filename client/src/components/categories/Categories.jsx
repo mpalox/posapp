64686380bb233205daba3d6000c71d1b054e1d08
mpalox <palomustafa@gmail.com>
2023-04-12 13:22:36 +0100

    kategore cekem goterme tamamlandi

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,30 +1,54 @@
-const Categories = () => {
-  return (
+import { useState } from "react";
+import "./style.css"
+import {PlusOutlined} from "@ant-design/icons"
+import { Form, Input, Modal,Button, message } from 'antd';
+const Categories = ({categories,setCategories}) => {
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const [form]=Form.useForm();
+    const onFinish = (values)=>{
+        try {
+            fetch("http://localhost:5000/api/categories/add-category",{
+                method:"POST",
+                body:JSON.stringify(values),
+                headers:{
+                    "Content-Type": "application/json; charset=UTF-8"
+                }
+            });
+            message.success("Kategori basariyla eklendi");
+            form.resetFields();
+            setCategories([...categories,values]);
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    return (
     <ul className="flex gap-4 md:flex-col text-lg">
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Tumu</span>
+            {categories.map((item)=>(
+                <li className="category-item" key={item._id}>
+                    <span>{item.title}</span>
+                </li>
+            ))}
+
+        <li className="category-item !bg-purple-800 hover:opacity-90" onClick={()=>setIsAddModalOpen(true)}>
+           <PlusOutlined className="md:text-2xl"/>
         </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Yiyecek</span>
-        </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Icecek</span>
-        </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Icecek</span>
-        </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Icecek</span>
-        </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Icecek</span>
-        </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Icecek</span>
-        </li>
-        <li className="text-center bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-            <span>Icecek</span>
-        </li>
+        <Modal  title="Yeni Kategori Ekle"
+                open={isAddModalOpen}
+                onCancel={()=>setIsAddModalOpen(false)}
+                footer={false}
+        >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+            <Form.Item  name="title" label="Kategori Ekle"
+                        rules={[{required:true,message:"Kategori Adi Bos gecilemez!"}]}>
+                <Input />
+            </Form.Item>
+
+            <Form.Item className="flex justify-end mb-0">
+                <Button type="primary" htmlType="submit">Olustur</Button>
+            </Form.Item>
+        </Form>
+      </Modal>
         
     </ul>
   )
