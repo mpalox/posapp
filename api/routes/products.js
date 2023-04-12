2c6823802d7cbdf8c087a1e7fc5be95957fc718f
mpalox <palomustafa@gmail.com>
2023-04-12 02:28:49 +0100

    tamalamalar devam

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,22 +1,22 @@
-const Category = require("../models/Category.js");
+const Product = require("../models/Product.js");
 const express = require("express");
 const router = express.Router();
 
 //! Get All
 router.get("/get-all", async (req, res) => {
   try {
-    const categories = await Category.find();
-    res.status(200).json(categories);
+    const products = await Product.find();
+    res.status(200).json(products);
   } catch (error) {
     console.log(error);
   }
 });
 
 //! Add
-router.post("/add-category", async (req, res) => {
+router.post("/add-product", async (req, res) => {
   try {
-    const newCategory = new Category(req.body);
-    await newCategory.save();
+    const newProduct = new Product(req.body);
+    await newProduct.save();
     res.status(200).json("Item added successfully");
   } catch (error) {
     res.status(400).json(error);
@@ -25,9 +25,9 @@ router.post("/add-category", async (req, res) => {
 module.exports = router;
 
 //! Update
-router.put("/update-category", async (req, res) => {
+router.put("/update-product", async (req, res) => {
   try {
-    await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
+    await Product.findOneAndUpdate({ _id: req.body.productId }, req.body);
     res.status(200).json("Item updated successfully.");
   } catch (error) {
     console.log(error);
@@ -35,9 +35,9 @@ router.put("/update-category", async (req, res) => {
 });
 
 //! Delete
-router.delete("/delete-category", async (req, res) => {
+router.delete("/delete-product", async (req, res) => {
   try {
-    await Category.findOneAndDelete({ _id: req.body.categoryId });
+    await Product.findOneAndDelete({ _id: req.body.productId });
     res.status(200).json("Item deleted successfully.");
   } catch (error) {
     console.log(error);
--- api/server.js
+++ api/server.js
@@ -15,6 +15,9 @@ const port = 5000;
 
 //* routes
 const categoryRoute = require("./routes/categories.js");
+const productRoute = require("./routes/products.js");
+const billRoute = require("./routes/bills.js");
+const authRoute = require("./routes/auth.js");
 
 dotEnv.config();
 
@@ -35,9 +38,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
+app.use("/api/bills", billRoute);
+app.use("/api/auth", authRoute);
 
 // app.get("/", (req, res) => res.send("Hello world"));
-
 app.listen(port, () => {
   connect();
   console.log(`Example app listening on port ${port}`);
